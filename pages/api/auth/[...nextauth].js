8f9d09646e1a3d9415b2f2b0a8ffcc557b6bd932
DesarolloTecno <roboticaeinovacion@gmail.com>
2023-06-28 11:45:21 -0300

    turing

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,39 +1,43 @@
-import clientPromise from '@/lib/mongodb'
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
-import NextAuth, { getServerSession } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
 
-
-const nextAuthUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
-const adminEmails = ['yosuanmulti@gmail.com']
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
-  ],
-  adapter: MongoDBAdapter(clientPromise),
-  callbacks: {
-    session: ({ session, token, user }) => {
-      if (adminEmails.includes(session?.user?.email)) {
-        return session;
-      } else {
-        return false;
-      }
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET, // Agrega el secreto aquí
-};
-
-export default NextAuth(authOptions);
-
-export async function isAdminRequest(req, res) {
-  const session = await getServerSession(req, res, authOptions);
-  if (!adminEmails.includes(session?.user?.email)) {
-    res.status(401);
-    res.end();
-    throw 'not an admin';
+  import NextAuth, {getServerSession} from 'next-auth'
+  import GoogleProvider from 'next-auth/providers/google'
+  import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
+  import clientPromise from "@/lib/mongodb";
+  import {Admin} from "@/models/Admin";
+  import {mongooseConnect} from '@/lib/mongoose';
+  
+  async function isAdminEmail(email) {
+    mongooseConnect();
+    return !! (await Admin.findOne({email}));
   }
-}
+  
+  export const authOptions = {
+    secret: process.env.SECRET,
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET
+      }),
+    ],
+    adapter: MongoDBAdapter(clientPromise),
+    callbacks: {
+      session: async ({session,token,user}) => {
+        if (await isAdminEmail(session?.user?.email)) {
+          return session;
+        } else {
+          return false;
+        }
+      },
+    },
+  };
+  
+  export default NextAuth(authOptions);
+  
+  export async function isAdminRequest(req,res) {
+    const session = await getServerSession(req,res,authOptions);
+    if (!(await isAdminEmail(session?.user?.email))) {
+      res.status(401);
+      res.end();
+      throw 'not an admin';
+    }
+  }
